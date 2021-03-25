ab7d1947bd111ee5a775cb0fbe2c0923a0ca2a23
Mattymoo007 <mattymoo007@gmail.com>
2021-03-25 22:46:28 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '68f310d582ee83e73cd774c22ed3190f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '092be957-5f70-439f-89e7-f243fed97c78'
+};
